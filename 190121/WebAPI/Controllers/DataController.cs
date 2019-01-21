d16f6214228036a2dcc54cf913222a2fb998ea4f
JeongInHye <inhyea36@naver.com>
2019-01-21 10:32:38 +0900

    WebAPI 코드 추가_1

---
+++ 190121/WebAPI/Controllers/DataController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Modules;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        [Route("api/Select")]
+        [HttpPost]
+        public ActionResult<ArrayList> Select()
+        {
+            return Query.GetSelect();
+        }
+
+        [Route("api/Select")]
+        [HttpGet]
+        public ActionResult<ArrayList> Select1()
+        {
+            return Query.GetSelect();
+        }
+
+        [Route("api/Insert")]
+        [HttpPost]
+        public ActionResult<ArrayList> Insert([FromForm] Data data)
+        {
+            return Query.GetInsert(data);
+        }
+
+        [Route("api/Update")]
+        [HttpPost]
+        public ActionResult<ArrayList> Update([FromForm] Data test)
+        {
+            return Query.GetUpdate(test);
+        }
+
+        [Route("api/Delete")]
+        [HttpPost]
+        public ActionResult<ArrayList> Delete([FromForm] Data test)
+        {
+            return Query.GetDelete(test);
+        }
+    }
+}
---
+++ 190121/WebAPI/Modules/Data.cs
@@ -0,0 +1,98 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Modules
+{
+    public class Data
+    {
+        public string mNo
+        {
+            set;
+            get;
+        }
+
+        public string nTitle
+        {
+            set;
+            get;
+        }
+
+        public string nContents
+        {
+            set;
+            get;
+        }
+        public string nNo
+        {
+            set;
+            get;
+        }
+    }
+
+    public static class Query
+    {
+        public static ArrayList GetInsert(Data data)
+        {
+            MYsql my = new MYsql();
+            string sql = string.Format("INSERT INTO Notice (mNo,nTitle,nContents) VALUES ({0},'{1}','{2}');", data.mNo, data.nTitle, data.nContents);
+            if (my.NonQuery(sql))
+            {
+                return GetSelect();
+            }
+            else
+            {
+                return new ArrayList();
+            }
+        }
+
+        public static ArrayList GetUpdate(Data data)
+        {
+            MYsql my = new MYsql();
+            string sql = string.Format("update Notice SET nTitle = '{0}',nContents='{1}' WHERE nNo = '{2}';", data.nTitle, data.nContents, data.nNo);
+            if (my.NonQuery(sql))
+            {
+                return GetSelect();
+            }
+            else
+            {
+                return new ArrayList();
+            }
+        }
+
+        public static ArrayList GetDelete(Data data)
+        {
+            MYsql my = new MYsql();
+            string sql = string.Format("update Notice SET delYn = 'Y' WHERE nNo = '{0}';", data.nNo);
+            if (my.NonQuery(sql))
+            {
+                return GetSelect();
+            }
+            else
+            {
+                return new ArrayList();
+            }
+        }
+
+        public static ArrayList GetSelect()
+        {
+            MYsql my = new MYsql();
+            string sql = string.Format("select n.nNo,n.nTitle,n.nContents,m.mName,DATE_FORMAT(n.regDate, '%Y-%m-%d') as regDate,DATE_FORMAT(n.modDate, '%Y-%m-%d') as modDate from Notice as n  inner join Member as m  on (n.mNo = m.mNo and m.delYn = 'N') where n.delYn = 'N';");
+            MySqlDataReader sdr = my.Reader(sql);
+            ArrayList list = new ArrayList();
+            while (sdr.Read())
+            {
+                Hashtable ht = new Hashtable();
+                for (int i = 0; i < sdr.FieldCount; i++)
+                {
+                    ht.Add(sdr.GetName(i), sdr.GetValue(i));
+                }
+                list.Add(ht);
+            }
+            return list;
+        }
+    }
+}
